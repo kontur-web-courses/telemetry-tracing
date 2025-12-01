f1a522481717ed91ee841903cc577986b787c6a0
EvGeniusM <thezhekychan@gmail.com>
2025-12-01 15:02:50 +0500

    complete-task

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -8,6 +8,30 @@ builder.Services.AddSingleton<SomeWorker>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", (
+    [FromServices] ActivitySource activitySource,
+    [FromServices] SomeWorker worker) =>
+{
+    using var activity = activitySource.StartActivity("HelloWorld");
+    
+    activity?.AddTag("http.method", "GET");
+    activity?.AddTag("http.route", "/");
+    activity?.AddTag("custom.tag", "custom_value");
+    activity?.AddTag("environment", app.Environment.EnvironmentName);
+    activity?.AddTag("timestamp", DateTime.UtcNow.ToString("o"));
+    
+    try
+    {
+        worker.DoSomeWork();
+        
+        return "Hello World!";
+    }
+    catch (Exception ex)
+    {
+        activity?.SetStatus(ActivityStatusCode.Error);
+        activity?.AddTag("error.message", ex.Message);
+        activity?.AddTag("error.stacktrace", ex.StackTrace);
+        throw;
+    }
+});
 app.Run();
