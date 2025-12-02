ed60c1eb24902df8b096e8d10eb25e1954912a55
HardPrecision <mikhreninroman@mail.ru>
2025-12-02 15:19:11 +0500

    done

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -2,12 +2,20 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using tracing_test_service;
 
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenTelemetry().AddOtelTracing();
 builder.Services.AddSingleton<SomeWorker>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] ActivitySource activitySource, [FromServices] SomeWorker worker) => {
+    using var activity = activitySource.StartActivity("HelloWorld");
+    
+    activity?.AddTag("custom.tag", "super-tag");
+    worker.DoSomeWork();
+
+    return "Hello World!";
+});
 
 app.Run();
