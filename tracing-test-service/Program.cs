08ffdccb78f5d1895e1bdf66df199468bc6d6684
Andrey <andrey_vol01@list.ru>
2025-12-02 16:22:47 +0500

    Topor

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -1,13 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Trace;
 using tracing_test_service;
+using OpenTelemetry.Exporter.Jaeger;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var activitySource = new ActivitySource("TestService");
+builder.Services.AddSingleton(activitySource);
+
 builder.Services.AddOpenTelemetry().AddOtelTracing();
+
 builder.Services.AddSingleton<SomeWorker>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/",
+    (
+        [FromServices] ActivitySource source,
+        [FromServices] SomeWorker worker) =>
+    {
+        using var activity = source.StartActivity("HelloWorld");
+
+        activity?.AddTag("Custom", "Hello");
+        activity?.AddTag("Time", DateTime.Now);
+
+        worker.DoSomeWork();
+
+        return "ok";
+    });
 
 app.Run();
