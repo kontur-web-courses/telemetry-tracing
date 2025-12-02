4e47bf204d2bbd56f1fc057a62c8b5339ed5cc40
ILYA <ylmaskylov-ilia@mail.ru>
2025-12-02 15:16:16 +0500

    done52

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -3,11 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using tracing_test_service;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(new ActivitySource("tracing_test_service"));
 builder.Services.AddOpenTelemetry().AddOtelTracing();
 builder.Services.AddSingleton<SomeWorker>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] ActivitySource activitySource, [FromServices] SomeWorker worker) =>
+{
+    using var activity = activitySource.StartActivity("HelloWorld");
+
+    activity?.SetTag("custom_shaslichok", "kurganmash");
+
+    worker.DoSomeWork();
+
+    return "Hello World!";
+});
 
 app.Run();
