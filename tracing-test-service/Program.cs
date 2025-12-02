bf66eebdb3a5809bd9d0f4f90f8e87d9415a7aba
Grinik767 <grigorikiseleff@yandex.ru>
2025-12-02 15:18:35 +0500

    finish

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -5,9 +5,19 @@ using tracing_test_service;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenTelemetry().AddOtelTracing();
 builder.Services.AddSingleton<SomeWorker>();
+builder.Services.AddSingleton(new ActivitySource("HelloWorld"));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (
+    [FromServices] ActivitySource activitySource,
+    [FromServices] SomeWorker worker) =>
+{
+    using var activity = activitySource.StartActivity("HelloWorld");
+    activity.AddTag("абоба", "1");
+
+    worker.DoSomeWork();
+    return "Hello World!";
+});
 
 app.Run();
