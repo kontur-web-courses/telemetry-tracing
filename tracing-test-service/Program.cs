e17e03f23f7b899e4e7a4adae4605a837391416c
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-12-08 00:45:58 +0500

    123

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -5,9 +5,21 @@ using tracing_test_service;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenTelemetry().AddOtelTracing();
 builder.Services.AddSingleton<SomeWorker>();
+builder.Services.AddSingleton(new ActivitySource("tracing_test_service"));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet(
+    "/",
+    ([FromServices] ActivitySource activitySource, [FromServices] SomeWorker worker) =>
+    {
+        using var activity = activitySource.StartActivity("HelloWorld");
+        activity?.SetTag("custom.tag", "hello-world");
+        activity?.SetTag("worker.status", "started");
+
+        worker.DoSomeWork();
+
+        return "Hello World!";
+    });
 
 app.Run();
