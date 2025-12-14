60a8f13cf535c4884880b7a68889e8f09023dac0
Александр Лысенко <alexndr.lysenko@gmail.com>
2025-12-14 20:04:13 +0500

    +

--- tracing-test-service/Program.cs
+++ tracing-test-service/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddSingleton<SomeWorker>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] ActivitySource activitySource, [FromServices] SomeWorker worker) =>
+{
+    using var activity = activitySource.StartActivity("HelloWorld");
+
+    activity?.SetTag("custom.tag", "my-epic-value");
+    activity?.SetTag("http.method", "GET");
+
+    worker.DoSomeWork();
+
+    return "Hello World!";
+});
 
 app.Run();
